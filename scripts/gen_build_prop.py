642727fc0742f55d0f22f5001cae94368881cb9c
finish0314 <finish21190@gmail.com>
2026-01-25 23:24:13 +0800

    gen_build_prop: Add Soc prop and device name prop
    Change-Id: Idc0992a47dbc96adda9f6c0de4ce91fada514964

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -129,6 +129,8 @@ def parse_args():
 
   config["LineageDesc"] = config["BuildDesc"]
   config["LineageDevice"] = config["DeviceName"]
+  config["AviumSocName"] = ""
+  config["AviumDeviceName"] = ""
 
   if config["BuildNumber"].startswith("eng."):
     config["BuildNumber"] = config["DateUtc"]
@@ -248,7 +250,8 @@ def generate_build_info(args):
   # Only add _asan for a sanitized build if it isn't already a part of the
   # flavor (via a dedicated lunch config for example).
   print(f"ro.build.flavor={config['BuildFlavor']}")
-
+  print(f"ro.avium.soc.model={config['AviumSocName']}")
+  print(f"ro.avium.device.codename={config['AviumDeviceName']}")
   print(f"ro.lineage.device={config['LineageDevice']}")
 
   # These values are deprecated, use "ro.product.cpu.abilist"
